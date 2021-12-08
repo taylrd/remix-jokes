383e9512a9656153b6da6aee1a1fbd828d4bb779
Corey Taylor <corey@taylrd.com>
2021-12-08 13:45:42 -0500

    init repo

---
+++ app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,132 @@
+import type {
+  LoaderFunction,
+  ActionFunction,
+  MetaFunction
+} from "remix";
+import {
+  Link,
+  useLoaderData,
+  useCatch,
+  redirect,
+  useParams
+} from "remix";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server";
+import { requireUserId, getUserId } from "~/utils/session.server";
+
+export let meta: MetaFunction = ({
+  data
+}: {
+  data: LoaderData | undefined;
+}) => {
+  if (!data) {
+    return {
+      title: "No joke",
+      description: "No joke found"
+    };
+  }
+  return {
+    title: `"${data.joke.name}" joke`,
+    description: `Enjoy the "${data.joke.name}" joke and much more`
+  };
+};
+
+type LoaderData = { joke: Joke, isOwner: boolean };
+
+export let loader: LoaderFunction = async ({ request, params }) => {
+  let userId = await getUserId(request);
+  let joke = await db.joke.findUnique({
+    where: { id: params.jokeId }
+  });
+  if (!joke) {
+    throw new Response("What a joke! Not found.", {
+      status: 404
+    });
+  }
+  let data: LoaderData = { joke, isOwner: userId === joke.jokesterId };
+  return data;
+};
+
+export let action: ActionFunction = async ({
+  request,
+  params
+}) => {
+  let form = await request.formData();
+  if (form.get("_method") === "delete") {
+    let userId = await requireUserId(request);
+    let joke = await db.joke.findUnique({
+      where: { id: params.jokeId }
+    });
+    if (!joke) {
+      throw new Response(
+        "Can't delete what does not exist",
+        { status: 404 }
+      );
+    }
+    if (joke.jokesterId !== userId) {
+      throw new Response(
+        "Pssh, nice try. That's not your joke",
+        {
+          status: 401
+        }
+      );
+    }
+    await db.joke.delete({ where: { id: params.jokeId } });
+    return redirect("/jokes");
+  }
+};
+
+export default function JokeRoute() {
+  let data = useLoaderData<LoaderData>();
+
+  return (
+    <div>
+      <p>Here's your hilarious joke:</p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
+      {data.isOwner ? (
+        <form method="post">
+          <input
+            type="hidden"
+            name="_method"
+            value="delete"
+          />
+          <button type="submit" className="button">
+            Delete
+          </button>
+        </form>
+      ) : null}
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  let caught = useCatch();
+  let params = useParams();
+  switch (caught.status) {
+    case 404: {
+      return (
+        <div className="error-container">
+          Huh? What the heck is {params.jokeId}?
+        </div>
+      );
+    }
+    case 401: {
+      return (
+        <div className="error-container">
+          Sorry, but {params.jokeId} is not your joke.
+        </div>
+      );
+    }
+    default: {
+      throw new Error(`Unhandled error: ${caught.status}`);
+    }
+  }
+}
+
+export function ErrorBoundary() {
+  let { jokeId } = useParams();
+  return (
+    <div className="error-container">{`There was an error loading joke by the id ${jokeId}. Sorry.`}</div>
+  );
+}
---
+++ app/routes/jokes/new.tsx
@@ -0,0 +1,167 @@
+import type { ActionFunction, LoaderFunction } from "remix";
+import {
+  useActionData,
+  redirect,
+  useCatch,
+  Link,
+  Form
+} from "remix";
+import { db } from "~/utils/db.server";
+import {
+  requireUserId,
+  getUserId
+} from "~/utils/session.server";
+
+export let loader: LoaderFunction = async ({ request }) => {
+  let userId = await getUserId(request);
+  if (!userId) {
+    throw new Response("Unauthorized", { status: 401 });
+  }
+  return {};
+};
+
+function validateJokeContent(content: string) {
+  if (content.length < 10) {
+    return `That joke is too short`;
+  }
+}
+
+function validateJokeName(name: string) {
+  if (name.length < 2) {
+    return `That joke's name is too short`;
+  }
+}
+
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    name: string | undefined;
+    content: string | undefined;
+  };
+  fields?: {
+    name: string;
+    content: string;
+  };
+};
+
+export let action: ActionFunction = async ({
+  request
+}): Promise<Response | ActionData> => {
+  let userId = await requireUserId(request);
+  let form = await request.formData();
+  let name = form.get("name");
+  let content = form.get("content");
+  if (
+    typeof name !== "string" ||
+    typeof content !== "string"
+  ) {
+    return { formError: `Form not submitted correctly.` };
+  }
+
+  let fieldErrors = {
+    name: validateJokeName(name),
+    content: validateJokeContent(content)
+  };
+  let fields = { name, content };
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return { fieldErrors, fields };
+  }
+
+  let joke = await db.joke.create({
+    data: { ...fields, jokesterId: userId }
+  });
+  return redirect(`/jokes/${joke.id}`);
+};
+
+export default function NewJokeRoute() {
+  let actionData = useActionData<ActionData | undefined>();
+
+  return (
+    <div>
+      <p>Add your own hilarious joke</p>
+      <Form method="post">
+        <div>
+          <label>
+            Name:{" "}
+            <input
+              type="text"
+              defaultValue={actionData?.fields?.name}
+              name="name"
+              aria-invalid={
+                Boolean(actionData?.fieldErrors?.name) ||
+                undefined
+              }
+              aria-describedby={
+                actionData?.fieldErrors?.name
+                  ? "name-error"
+                  : undefined
+              }
+            />
+          </label>
+          {actionData?.fieldErrors?.name ? (
+            <p
+              className="form-validation-error"
+              role="alert"
+              id="name-error"
+            >
+              {actionData.fieldErrors.name}
+            </p>
+          ) : null}
+        </div>
+        <div>
+          <label>
+            Content:{" "}
+            <textarea
+              defaultValue={actionData?.fields?.content}
+              name="content"
+              aria-invalid={
+                Boolean(actionData?.fieldErrors?.content) ||
+                undefined
+              }
+              aria-describedby={
+                actionData?.fieldErrors?.content
+                  ? "content-error"
+                  : undefined
+              }
+            />
+          </label>
+          {actionData?.fieldErrors?.content ? (
+            <p
+              className="form-validation-error"
+              role="alert"
+              id="content-error"
+            >
+              {actionData.fieldErrors.content}
+            </p>
+          ) : null}
+        </div>
+        <div>
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  let caught = useCatch();
+
+  if (caught.status === 401) {
+    return (
+      <div className="error-container">
+        <p>You must be logged in to create a joke.</p>
+        <Link to="/login">Login</Link>
+      </div>
+    );
+  }
+}
+
+export function ErrorBoundary() {
+  return (
+    <div className="error-container">
+      Something unexpected went wrong. Sorry about that.
+    </div>
+  );
+}
